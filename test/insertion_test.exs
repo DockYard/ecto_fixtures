e6271c098bcb0e08c16b577330785968829725a6
Brian Cardarella <bcardarella@gmail.com>
2015-08-30 22:35:55 -0400

    Ensure the model schema is in the row data

--- test/insertion_test.exs
+++ test/insertion_test.exs
@@ -35,18 +35,24 @@ defmodule EctoFixtures.InsertionTest do
     %{cars: cars, owners: owners, pets: pets} = fixtures(:insert_1, insert: false)
 
     assert length(BaseRepo.all(Car)) == 0
+    assert %Car{} = cars.nissan
     assert cars.nissan.color == "black"
     assert cars.nissan.owner_id == owners.brian.id
+    assert %Car{} = cars.tesla
     assert cars.tesla.color == "red"
     assert cars.tesla.owner_id == owners.stephanie.id
+    assert %Car{} = cars.toyota
     assert cars.toyota.color == "white"
     assert cars.toyota.owner_id == owners.stephanie.id
 
     assert length(BaseRepo.all(Owner)) == 0
+    assert %Owner{} = owners.brian
     assert owners.brian.name == "Brian"
+    assert %Owner{} = owners.stephanie
     assert owners.stephanie.name == "Stephanie"
 
     assert length(BaseRepo.all(Pet)) == 0
+    assert %Pet{} = pets.boomer
     assert pets.boomer.name == "Boomer"
     assert pets.boomer.owner_id == owners.brian.id
 
