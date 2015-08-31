d21eb94e8491aa88c0fb7ffedf98628475756975
Brian Cardarella <bcardarella@gmail.com>
2015-08-30 21:39:04 -0400

    Allow data sets to be built without inserting

--- lib/ecto/fixtures.ex
+++ lib/ecto/fixtures.ex
@@ -2,14 +2,18 @@ defmodule EctoFixtures do
   def read(path), do: File.read!(path)
   def parse(content), do: EctoFixtures.Parser.parse(content)
   def condition(data), do: EctoFixtures.Conditioner.condition(data)
-  def insert(data), do: EctoFixtures.Insertion.insert(data)
+  def insert(data, can_insert), do: EctoFixtures.Insertion.insert(data, can_insert)
   def normalize(data), do: EctoFixtures.Normalizer.normalize(data)
 
   def fixtures(name) do
+    fixtures(name, insert: true)
+  end
+
+  def fixtures(name, [insert: can_insert]) do
     data = read("test/fixtures/#{name}.exs")
     |> parse
     |> condition
-    |> insert
+    |> insert(can_insert)
     |> normalize
   end
 end
--- test/insertion_test.exs
+++ test/insertion_test.exs
@@ -1,6 +1,6 @@
 defmodule EctoFixtures.InsertionTest do
   use EctoFixtures.Integration.Case
-  import EctoFixtures, only: [fixtures: 1]
+  import EctoFixtures, only: [fixtures: 1, fixtures: 2]
 
   test "properly inserts fixtures into the database" do
     fixtures(:insert_1)
@@ -29,5 +29,26 @@ defmodule EctoFixtures.InsertionTest do
     owners = BaseRepo.all(Owner)
 
     assert length(owners) == 0
+  end
+
+  test "does not insert any rows when `insert: false` is passed to `fixtures/2" do
+    %{cars: cars, owners: owners, pets: pets} = fixtures(:insert_1, insert: false)
+
+    assert length(BaseRepo.all(Car)) == 0
+    assert cars.nissan.color == "black"
+    assert cars.nissan.owner_id == owners.brian.id
+    assert cars.tesla.color == "red"
+    assert cars.tesla.owner_id == owners.stephanie.id
+    assert cars.toyota.color == "white"
+    assert cars.toyota.owner_id == owners.stephanie.id
+
+    assert length(BaseRepo.all(Owner)) == 0
+    assert owners.brian.name == "Brian"
+    assert owners.stephanie.name == "Stephanie"
+
+    assert length(BaseRepo.all(Pet)) == 0
+    assert pets.boomer.name == "Boomer"
+    assert pets.boomer.owner_id == owners.brian.id
+
   end
 end
